f72d0c7e04fca267aee7f49cfa7da37e0f0aeb22
rasol kalantari <imrkalantari@gmail.com>
2025-10-27 11:18:52 +0330

    change name file

---
+++ consumer2/cunsomertwo.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func main() {
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:     []string{"localhost:9092"},
+		Topic:       "orders",
+		GroupID:     "order-consumer-group-test4",
+		StartOffset: kafka.LastOffset,
+	})
+	defer reader.Close()
+
+	log.Println("reader two started...!")
+
+	for {
+		var ctx, cancle = context.WithTimeout(context.Background(), 5*time.Second)
+		msg, err := reader.ReadMessage(ctx)
+		cancle()
+
+		if err != nil {
+			log.Println("No new message...")
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		log.Printf("Message received: partition=%d offset=%d key=%s value=%s\n", msg.Partition, msg.Offset, string(msg.Key), string(msg.Value))
+	}
+
+}
